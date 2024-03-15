868b2e8ca3507d9e7690857854707070451f5570
SAFEluren <safeluren@Outlook.com>
2024-03-16 01:11:35 +0800

    first commit - ver 0.0.1

---
+++ src/commands/score.ts
@@ -0,0 +1,45 @@
+import { Context, Logger } from 'koishi';
+import { } from 'koishi-plugin-puppeteer'
+
+export function score(ctx: Context) {
+  ctx.command('score', '查看局内比分', { authority: 0 })
+    .alias("比分","分数")
+    .action(async ({ session }) => {
+      const apiurl = `http://127.0.0.1:10086/Server/GetServerData`;
+      try {
+        const respone = await ctx.http.get(apiurl)
+
+        let message: string = `${respone.message}`
+        message += `\n服务器名: ${respone.data.name}`
+        message += `\nGameID: ${respone.data.gameId}`
+        message += `\n--------------------`
+        message += `\n游戏时间: ${respone.data.gameTime}`
+        message += `\n当前地图: ${respone.data.mapName2}[${respone.data.gameMode2}]`
+        // 判断是否为征服
+        if (respone.data.gameMode == "Conquest0") {
+          message += `\n${respone.data.team1.name}: ${respone.data.team1.allScore} / ${respone.data.team1.maxScore}`
+          message += `\n${respone.data.team2.name}: ${respone.data.team2.allScore} / ${respone.data.team2.maxScore}`
+
+          // 分差计算
+          if (respone.data.team1.allScore > respone.data.team2.allScore) {
+            let score_difference: number = respone.data.team1.allScore - respone.data.team2.allScore
+            message += `\n分差: ${score_difference}`
+          } else if (respone.data.team2.allScore > respone.data.team1.allScore) {
+            let score_difference: number = respone.data.team2.allScore - respone.data.team1.allScore
+            message += `\n分差: ${score_difference}`
+          } else if (respone.data.team1.allScore - respone.data.team2.allScore == 0){
+            message += `\n分差: 0`
+          }
+
+        } else {
+          message += `\n当前模式为[${respone.data.gameMode2}]，无比分数据。`
+        }
+        message += `\n--------------------`
+        message += `\n${respone.timestamp}`
+
+        return message
+      } catch (error) {
+        return "访问API失败。";
+      }
+    });
+}
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,64 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema, Session } from 'koishi'
+import { score } from './commands/score'
+import { } from 'koishi-plugin-cron';
 
 export const name = 'bf1-servermanagertools'
-
-export interface Config {}
-
-export const Config: Schema<Config> = Schema.object({})
-
-export function apply(ctx: Context) {
-  // write your plugin here
+export const inject = {
+  required: ['cron']
 }
+export interface Config {
+  pushGroupIDs: string[]
+}
+
+export const Config: Schema<Config> = Schema.object({
+  pushGroupIDs: Schema.array(String).description(`推送的目标QQ群`),
+})
+
+export function apply(ctx: Context,config: Config) {
+  const logger = ctx.logger(name)
+  const {pushGroupIDs} = config
+  ctx.cron('*/2 * * * *', async () => {
+    logger.info(`into cron`)
+    // 每隔 2 分钟执行
+    const apiurl = `http://127.0.0.1:10086/Server/GetServerData`;
+    try {
+      const respone = await ctx.http.get(apiurl)
+
+      let message: string = `[比分推送 间隔2分钟]`
+      message += `\n游戏时间: ${respone.data.gameTime}`
+      // 判断是否为征服
+      if (respone.data.gameMode == "Conquest0") {
+        message += `\n${respone.data.team1.name}:${respone.data.team1.allScore} / ${respone.data.team2.name}:${respone.data.team2.allScore}`
+        // 分差计算
+        if (respone.data.team1.allScore > respone.data.team2.allScore) {
+          let score_difference: number = respone.data.team1.allScore - respone.data.team2.allScore
+          message += `\n当前地图: ${respone.data.mapName2}[${respone.data.gameMode2}]    分差: ${score_difference}`
+        } else if (respone.data.team2.allScore > respone.data.team1.allScore) {
+          let score_difference: number = respone.data.team2.allScore - respone.data.team1.allScore
+          message += `\n当前地图: ${respone.data.mapName2}[${respone.data.gameMode2}]    分差: ${score_difference}`
+        } else if (respone.data.team1.allScore - respone.data.team2.allScore == 0) {
+          message += `\n当前地图: ${respone.data.mapName2}[${respone.data.gameMode2}]    分差: 0`
+        }
+
+      } else {
+        message == `\n当前模式为[${respone.data.gameMode2}]，无比分数据。`
+        return false
+      }
+      message += `\n--------------------`
+      message += `\n${respone.timestamp}`
+      for (const currentBot of ctx.bots) {
+        for (const groupId of pushGroupIDs) {
+            await currentBot.sendMessage(groupId, message);
+            logger.success(`群${groupId}已推送.(${respone.timestamp})`)
+          }
+        }
+    } catch (error) {
+      logger.error(`发生错误`)
+
+    }
+
+  })
+  score(ctx);
+
+}
+
